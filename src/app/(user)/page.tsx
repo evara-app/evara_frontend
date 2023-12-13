237cc8dab03758d17186e19123fe117f32e5bb9e
Farhan Ahmadi <farhanahmadiro7@gmail.com>
2023-12-14 01:37:34 +0330

    build structure of project

---
+++ src/app/(user)/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function page() {
+  return <div>سایت ایوارا</div>;
+}
+
+export default page;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,29 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
+const { fontFamily } = require("tailwindcss/defaultTheme");
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        homeBackground:
+          "linear-gradient(91deg,rgba(67,181,226,.7),rgba(136,197,71,.5)), url('/assets/img/navbarBg.jpg')",
+      },
+      colors: {
+        "green_blue": "#00bb7e",
+        "aqua-green": "#0ede9b",
+        "gray-default": "#4A4B4C",
+        "white-two": "#f8f8f8",
+      },
+      fontFamily: {
+        sans: ["var(--font-estedad)", ...fontFamily.sans],
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;

9f7cef6ae3b36bdc13df6d75c4232a8b1005e465
Farhan Ahmadi <farhanahmadiro7@gmail.com>
2024-09-18 10:50:06 +0330

    update images gallery structure

--- src/app/(profile)/profile/gallery/[slug]/GalleryComponent.jsx
+++ src/app/(profile)/profile/gallery/[slug]/GalleryComponent.jsx
@@ -5,6 +5,7 @@ import React, { useState, useRef, useEffect } from "react";
 //? import icons
 import { IoImagesOutline } from "react-icons/io5";
 import { FaTrashCan, FaTruckFieldUn } from "react-icons/fa6";
+import { FaCircle } from "react-icons/fa6";
 
 //? import service
 import { imageUpload } from "@/services/images";
@@ -37,8 +38,11 @@ function GalleryComponent({ DBImages }) {
       );
       locationsArray[`image${[image.id]}`] = Location;
       imagesLocation.push(Location);
+      setGalleryImages((prevstate) => [
+        ...prevstate,
+        { id: image.id, file: Location },
+      ]);
       if (imagesLocation.length === array.length) {
-        // handler(imagesLocation);
         setLocations(locationsArray);
       }
     });
@@ -94,6 +98,7 @@ function GalleryComponent({ DBImages }) {
       setGalleryImages((prevstate) => [...prevstate, { id: id, file: file }]);
     }
   };
+  console.log(primaryImages);
 
   return (
     <div>
@@ -211,59 +216,62 @@ function GalleryComponent({ DBImages }) {
             </div>
           </div>
         ))}
-        {imagesBlob.map((image) => (
-          <div
-            key={image.id}
-            className="image-upload-preview relative group"
-            style={{
-              backgroundImage: `url(${image.blob})`,
-            }}
-          >
-            <button
-              onClick={() => imageDelHandler(image.id)}
-              type="button"
-              className="absolute top-2 right-2 p-2 bg-gray-700/30 rounded z-10 hover:bg-gray-700/60 transition"
-            >
-              <FaTrashCan className="w-4 h-4 text-white" />
-            </button>
-            <div
-              id={`image_preview${image.id}`}
-              className="image-upload-preview-overlay bg-gray-200/80 z-30"
-            ></div>
-            <div className="image-upload-preview-overlay flex items-center justify-center">
-              <div
-                id={`image-upload-progress${image.id}`}
-                className="image-upload-preview__progress-overlay"
-              >
-                <div id={image.id} className="progress-bar__inner"></div>
-              </div>
-            </div>
-            {/* {mainImages.includes(locations[`image${image.id}`]) && (
-              <span className="absolute top-0 left-0 bg-green-blue text-white p-2 rounded-sm">
-                {Number(mainImages.indexOf(locations[`image${image.id}`])) + 1}
-              </span>
-            )} */}
-            <div className="absolute top-0 left-0 hidden group-hover:flex  backdrop-blur-sm w-full h-full items-center justify-center">
-              <button
-                className="bg-green-700/60 text-white p-2 rounded-md"
-                onClick={() =>
-                  selectMainImages(locations[`image${image.id}`], image)
-                }
-              >
-                {Object.values(primaryImages).includes(image.id)
-                  ? `Deselect image ${Object.values(primaryImages).indexOf(
-                      image.id
-                    )}`
-                  : `
-                select as image ${Object.keys(primaryImages).length + 1}
-                `}
-              </button>
-            </div>
-          </div>
-        ))}
       </div>
     </div>
   );
 }
 
 export default GalleryComponent;
+
+// {
+//   imagesBlob.map((image) => (
+//     <div
+//       key={image.id}
+//       className="image-upload-preview relative group"
+//       style={{
+//         backgroundImage: `url(${image.blob})`,
+//       }}
+//     >
+//       <button
+//         onClick={() => imageDelHandler(image.id)}
+//         type="button"
+//         className="absolute top-2 right-2 p-2 bg-gray-700/30 rounded z-10 hover:bg-gray-700/60 transition"
+//       >
+//         <FaTrashCan className="w-4 h-4 text-white" />
+//       </button>
+//       <div
+//         id={`image_preview${image.id}`}
+//         className="image-upload-preview-overlay bg-gray-200/80 z-30"
+//       ></div>
+//       <div className="image-upload-preview-overlay flex items-center justify-center">
+//         <div
+//           id={`image-upload-progress${image.id}`}
+//           className="image-upload-preview__progress-overlay"
+//         >
+//           <div id={image.id} className="progress-bar__inner"></div>
+//         </div>
+//       </div>
+//       {/* {mainImages.includes(locations[`image${image.id}`]) && (
+//             <span className="absolute top-0 left-0 bg-green-blue text-white p-2 rounded-sm">
+//               {Number(mainImages.indexOf(locations[`image${image.id}`])) + 1}
+//             </span>
+//           )} */}
+//       <div className="absolute top-0 left-0 hidden group-hover:flex  backdrop-blur-sm w-full h-full items-center justify-center">
+//         <button
+//           className="bg-green-700/60 text-white p-2 rounded-md"
+//           onClick={() =>
+//             selectMainImages(locations[`image${image.id}`], image)
+//           }
+//         >
+//           {Object.values(primaryImages).includes(image.id)
+//             ? `Deselect image ${Object.values(primaryImages).indexOf(
+//                 image.id
+//               )}`
+//             : `
+//               select as image ${Object.keys(primaryImages).length + 1}
+//               `}
+//         </button>
+//       </div>
+//     </div>
+//   ));
+// }
